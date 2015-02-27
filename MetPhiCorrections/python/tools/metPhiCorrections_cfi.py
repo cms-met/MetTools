26064888101bd75fe210ac94f98118e7ac95f4d8
schoef <robert.schoefbeck@cern.ch>
2015-02-27 09:28:24 +0100

    finalizing MET phi correction tools

---
+++ MetPhiCorrections/python/tools/metPhiCorrections_cfi.py
@@ -0,0 +1,23 @@
+    cms.PSet(
+      name=cms.string("h"),
+      type=cms.int32(1),
+      varType=cms.int32(0),
+      etaMin=cms.double(0),
+      etaMax=cms.double(7500),
+      fx=cms.string("(x*[0])+(sq(x)*[1])"),
+      px=cms.vdouble(-0.00100488080903,-2.52390571355e-07),
+      fy=cms.string("(x*[0])+(sq(x)*[1])"),
+      py=cms.vdouble(-0.00861386123702,4.62378996583e-07),
+    ),
+)
+    cms.PSet(
+      name=cms.string("h"),
+      type=cms.int32(1),
+      varType=cms.int32(0),
+      etaMin=cms.double(-2.7),
+      etaMax=cms.double(2.7),
+      fx=cms.string("(x*[0])+(sq(x)*[1])"),
+      px=cms.vdouble(-0.00100488080903,-2.52390571355e-07),
+      fy=cms.string("(x*[0])+(sq(x)*[1])"),
+      py=cms.vdouble(-0.00861386123702,4.62378996583e-07),
+    ),
