12e406eaf3e2ea9f5fe8a359d03e37943d466f9b
Lara <lara@cern.ch>
2015-09-13 00:33:56 +0200

    Now it takes also the channel (MuMu or EE) as input

--- MetPerformance/runPlotWithRatioJES.C
+++ MetPerformance/runPlotWithRatioJES.C
@@ -2,17 +2,17 @@
 #include "TSystem.h"
 #include "PlotWithRatioJES.C"
 
-void runPlotWithRatioJES(){
-//gSystem->CompileMacro("PlotWithRatio.C", "MuMu");
+void runPlotWithRatioJES(TString channel){
+//gSystem->CompileMacro("PlotWithRatio.C", channel);
 
 
-  testRatio("met_uPara_zlloverzll_pt_vs_zll_pt", "qt [GeV]", "MuMu");
-  testRatio( "met_uPara_zll+zll_pt_vs_zll_pt", "qt [GeV]","MuMu") ;
-  testRatio("met_uPerp_zll_vs_zll_pt","qt [GeV]", "MuMu");
-  testRatio("met_uPara_zll+zll_pt_vs_met_sumEt", "sumET [GeV]", "MuMu");
-  testRatio("met_uPerp_zll_vs_met_sumEt","sumET [GeV]", "MuMu");
-  testRatio("met_uPara_zll+zll_pt_vs_nVert","nvertex","MuMu") ; 
-  testRatio("met_uPerp_zll_vs_nVert","nvertex", "MuMu");
+  testRatio("met_uPara_zlloverzll_pt_vs_zll_pt", "qt [GeV]", channel);
+  testRatio( "met_uPara_zll+zll_pt_vs_zll_pt", "qt [GeV]",channel) ;
+  testRatio("met_uPerp_zll_vs_zll_pt","qt [GeV]", channel);
+  testRatio("met_uPara_zll+zll_pt_vs_met_sumEt", "sumET [TeV]", channel);
+  testRatio("met_uPerp_zll_vs_met_sumEt","sumET [TeV]", channel);
+  testRatio("met_uPara_zll+zll_pt_vs_nVert","nvertex",channel) ; 
+  testRatio("met_uPerp_zll_vs_nVert","nvertex", channel);
 
 }
 
