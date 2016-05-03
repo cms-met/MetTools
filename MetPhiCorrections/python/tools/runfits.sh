c32454d5505808262e42cbb62c9d6d9fc9f7a0e5
Ece Asilar <ece.asilar@cern.ch>
2016-05-03 18:05:45 +0200

    new files for fitting the MEx,y

---
+++ MetPhiCorrections/python/tools/runfits.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+#./fits.sh 76_WJets_Spring15  "WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1.root"                          "/METPhiCorr/plots/WJets_Spring15/"
+#./fits.sh 76_TTJets_Spring15 "TT_TuneCUETP8M1_13TeV-powheg-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1.root"                                        "/METPhiCorr/plots/TTJets_Spring15/"
+#./fits.sh 76_WJets_Fall15    "WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1.root"          "/METPhiCorr/plots/WJets_Fall15/"
+#./fits.sh 76_TTJets_Fall15   "TT_TuneCUETP8M1_13TeV-powheg-pythia8_RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext4-v1.root"                   "/METPhiCorr/plots/TTJets_Fall15/"
+./fits.sh 76_DY_Fall15        "DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1_V2.root"  "/METPhiCorr/plots/DY_Fall15/"
