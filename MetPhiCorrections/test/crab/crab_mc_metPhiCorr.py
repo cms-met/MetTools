b47908a61a96dde2359eede8814f8b9a89f818ef
sangilpark <sangilpark@knu.ac.kr>
2016-10-14 17:22:28 +0900

    haha

--- MetPhiCorrections/test/crab/crab_mc_metPhiCorr.py
+++ MetPhiCorrections/test/crab/crab_mc_metPhiCorr.py
@@ -1,28 +1,24 @@
 from CRABClient.UserUtilities import config , getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'METPhiCorr_analysis_TTJets_76'
-config.General.workArea = 'crab_projects'
+config.General.requestName = 'METPhiCorr_MC_DYJets'
+#config.General.requestName = 'METPhiCorr_MC_TTJets'
+#config.General.requestName = 'METPhiCorr_MC_WJetsToLNu'
+config.General.workArea = 'crab_projects_MetPhiCorr_mc'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'run_metPhiCorr.py'
+config.JobType.psetName = '../run_metPhiCorr_mc.py'
 
-#config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
-#config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1/MINIAODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12-v1/MINIAODSIM'
-config.Data.inputDataset = '/TT_TuneCUETP8M1_13TeV-powheg-pythia8/RunIIFall15MiniAODv2-PU25nsData2015v1_76X_mcRun2_asymptotic_v12_ext4-v1/MINIAODSIM'
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
+#config.Data.inputDataset = '/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
+#config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
+#config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1
-#config.Data.outLFNDirBase = '/store/user/%s/' % (getUsernameFromSiteDB())
-config.Data.outLFNDirBase = '/store/user/easilar/'
-config.Data.publication = True
-#config.Data.outputDatasetTag = 'CRAB3_DYJetsToLL_M-50_METPhi_analysis_v2'
-config.Data.outputDatasetTag = 'CRAB3_TTJets_TuneCUETP8M1_13TeV_76_METPhi_analysis_v2'
+config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/DYJets/'
+config.Data.publication = False
 
-config.Site.storageSite = 'T2_AT_Vienna'
-
-
+config.Site.storageSite = 'T2_KR_KNU'
