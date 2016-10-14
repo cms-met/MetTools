052e44d0158597fa85e810648c69c1b4de71a0cb
sangilpark <sangilpark@knu.ac.kr>
2016-10-14 17:27:19 +0900

    haha

--- MetPhiCorrections/test/crab/crab_mc_metPhiCorr.py
+++ MetPhiCorrections/test/crab/crab_mc_metPhiCorr.py
@@ -1,7 +1,7 @@
 from CRABClient.UserUtilities import config , getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'METPhiCorr_MC_DYJets'
+config.General.requestName = 'METPhiCorr_MC_DYJetsToLL'
 #config.General.requestName = 'METPhiCorr_MC_TTJets'
 #config.General.requestName = 'METPhiCorr_MC_WJetsToLNu'
 config.General.workArea = 'crab_projects_MetPhiCorr_mc'
@@ -10,6 +10,9 @@ config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '../run_metPhiCorr_mc.py'
+config.JobType.outputFiles = ['histo_mc_DYJetsToLL.root']
+#config.JobType.outputFiles = ['histo_mc_TTJets.root']
+#config.JobType.outputFiles = ['histo_mc_WJetsToLNu.root']
 
 config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
 #config.Data.inputDataset = '/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
@@ -18,7 +21,7 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 #config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1
-config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/DYJets/'
+config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/DYJetsToLL/'
 config.Data.publication = False
 
 config.Site.storageSite = 'T2_KR_KNU'
