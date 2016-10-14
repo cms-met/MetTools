39835b7e552bf0cff7315ed118cf25323ad85206
sangilpark <sangilpark@knu.ac.kr>
2016-10-14 19:33:27 +0900

    haha

--- MetPhiCorrections/test/crab/crab_data_metPhiCorr.py
+++ MetPhiCorrections/test/crab/crab_data_metPhiCorr.py
@@ -1,13 +1,13 @@
 from CRABClient.UserUtilities import config , getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'METPhiCorr_analysis_Data'
+config.General.requestName = 'METPhiCorr_Data_Run2016D'
 config.General.workArea = 'crab_projects_MetPhiCorr_data'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
-config.JobType.psetName = 'run_metPhiCorr.py'
+config.JobType.psetName = '../run_metPhiCorr_data.py'
 config.JobType.maxJobRuntimeMin = 2750
 config.JobType.outputFiles      = ['histo_data_Run2016D.root']
 
@@ -17,6 +17,7 @@ config.Data.inputDBS = 'global'
 config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 10
 config.Data.lumiMask = 'https://cms-service-dqm.web.cern.ch/cms-service-dqm/CAF/certification/Collisions16/13TeV/Cert_271036-277933_13TeV_PromptReco_Collisions16_JSON.txt'
+#config.Data.lumiMask = './json/Cert_271036-277933_13TeV_PromptReco_Collisions16_JSON.txt'
 config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/Data/Run2016D/'
 config.Data.publication = False
 
--- MetPhiCorrections/test/crab/crab_mc_metPhiCorr.py
+++ MetPhiCorrections/test/crab/crab_mc_metPhiCorr.py
@@ -1,27 +1,29 @@
 from CRABClient.UserUtilities import config , getUsernameFromSiteDB
 config = config()
 
-config.General.requestName = 'METPhiCorr_MC_DYJetsToLL'
+#config.General.requestName = 'METPhiCorr_MC_DYJetsToLL'
 #config.General.requestName = 'METPhiCorr_MC_TTJets'
-#config.General.requestName = 'METPhiCorr_MC_WJetsToLNu'
+config.General.requestName = 'METPhiCorr_MC_WJetsToLNu'
 config.General.workArea = 'crab_projects_MetPhiCorr_mc'
 config.General.transferOutputs = True
 config.General.transferLogs = True
 
 config.JobType.pluginName = 'Analysis'
 config.JobType.psetName = '../run_metPhiCorr_mc.py'
-config.JobType.outputFiles = ['histo_mc_DYJetsToLL.root']
+#config.JobType.outputFiles = ['histo_mc_DYJetsToLL.root']
 #config.JobType.outputFiles = ['histo_mc_TTJets.root']
-#config.JobType.outputFiles = ['histo_mc_WJetsToLNu.root']
+config.JobType.outputFiles = ['histo_mc_WJetsToLNu.root']
 
-config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
+#config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
 #config.Data.inputDataset = '/TTJets_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16RAWAODSIM_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
-#config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
+config.Data.inputDataset = '/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring16MiniAODv2-PUSpring16_80X_mcRun2_asymptotic_2016_miniAODv2_v0-v1/MINIAODSIM'
 config.Data.inputDBS = 'global'
 config.Data.splitting = 'FileBased'
 #config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 1
-config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/DYJetsToLL/'
+#config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/DYJetsToLL/'
+#config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/TTJets/'
+config.Data.outLFNDirBase = '/store/user/spak/MetPhiCorrection/MC/WJetsToLNu/'
 config.Data.publication = False
 
 config.Site.storageSite = 'T2_KR_KNU'
