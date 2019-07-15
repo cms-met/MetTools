e0c25ed2e4fb9ba340760d27418ccbef013b52f0
Nicolas Postiau <npostiau@ulb.ac.be>
2019-07-15 11:42:43 +0200

    Update scripts for 2018

--- MetPhiCorrections/python/tools/runfits.sh
+++ MetPhiCorrections/python/tools/runfits.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 
-./fits.sh Data_76X "/data/easilar/METPhiCorr/RootFiles/Data_76X/DoubleMuon_Run2015D_16Dec2015_v1_MINIAOD.root"  "/afs/hephy.at/user/e/easilar/www/METPhiCorr/Data_76X/" multiplicity
+#./fits.sh Data_76X "/data/easilar/METPhiCorr/RootFiles/Data_76X/DoubleMuon_Run2015D_16Dec2015_v1_MINIAOD.root"  "/afs/hephy.at/user/e/easilar/www/METPhiCorr/Data_76X/" multiplicity
+./fits.sh Data_102X "/user/npostiau/CMSSW_10_2_11_MetTools/src/MetTools/MetPhiCorrections/test/histo_DataDoubleMu2018C.root"  "/user/npostiau/public_html/MetPhi/Data_102X/" multiplicity
 
 #./fits.sh 76_WJets_Spring15 "/data/easilar/METPhiCorr/RootFiles/WJets_Spring15/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1.root"                      "/afs/hephy.at/user/e/easilar/www/METPhiCorr/WJets_Spring15/"
 #./fits.sh 76_WJets_Spring15 "/data/easilar/METPhiCorr/RootFiles/WJets_Spring15/WJetsToLNu_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8_RunIISpring15MiniAODv2-74X_mcRun2_asymptotic_v2-v1.root"                "deleteme/"  ngoodVertices & 
